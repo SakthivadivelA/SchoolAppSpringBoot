46222e7834f15ee9687ae3895f9766e985e501b7
SakthivadivelA <sakthivadivel.a@gmail.com>
2021-08-08 21:52:42 +0530

    Create StundentDTO.java
    Added StudentDTO

---
+++ src/main/java/com/benew/SchoolApp/DTO/StundentDTO.java
@@ -0,0 +1,24 @@
+package com.benew.SchoolApp.DTO;
+
+public class StundentDTO {
+
+	Long id;
+	String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
